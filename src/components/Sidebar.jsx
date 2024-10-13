37fe154feaa5fd8af601e875245e0342c7941eb0
Ankitaa28 <vishwakarma102ankita@gmail.com>
2024-10-13 22:53:42 +0530

    youtube-clone

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
-import { GoHomeFill } from "react-icons/go";
-
+import { MdHomeFilled,
+    MdOutLineSlowMotionVideo,
+    MdSubscriptions,
+    MdOutlineVideoLibrary,
+    MdHistory,
+    MdOutlineWatchLater
+ } from "react-icons/md";
+ import { PiThumbsUp } from "react-icons/pi";
 const Sidebar = () => {
+    const mainLinks=[
+        {
+            icon:<MdHomeFilled/>,
+            name: "Home"
+        },
+        {
+            icon:<MdOutLineSlowMotionVideo/>,
+            name: "Shorts"
+        },
+        {
+            icon:<MdSubscriptions/>,
+            name: "Subscription"
+        }
+    ]
+    const otherLink=[
+        {
+            icon:<MdOutlineVideoLibrary/>,
+            name: "Library"
+        },
+        {
+            icon:<MdHistory/>,
+            name: "History"
+        },
+        {
+            icon:<MdOutlineWatchLater/>,
+            name: "Watch Later"
+        },
+        {
+            icon:< PiThumbsUp />,
+            name: "Like"
+        }
+    ]
+
   return (
     <div>
-      <GoHomeFill/>
+      <ul>
+        {mainLinks.map(({icon,name})=>{
+            
+            return <li key={name}>
+                <a href="#">{icon}</a>
+            </li>
+        })}
+      </ul>
     </div>
   )
 }
